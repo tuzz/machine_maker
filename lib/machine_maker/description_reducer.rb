d7aa5469d4c6f7f0cedb653cbe811893af67a6e1
Chris Patuzzo <chris@patuzzo.co.uk>
2017-01-01 16:28:28 +0000

    Reduce transitions to CNF
    Each transition:
    
      - Has one from state
      - Has one to state
      - Has one read symbol
      - Has one write symbol
    
    - Transitions are deterministic, i.e. there is
    zero or one transition per from/read pair

---
+++ lib/machine_maker/description_reducer.rb
@@ -0,0 +1,86 @@
+class DescriptionReducer
+  def self.reduce(**params)
+    new(**params).reduce
+  end
+
+  attr_accessor :transitions, :symbols, :states, :io
+
+  def initialize(transitions:, symbols:, states:, io:)
+    self.transitions = transitions
+    self.symbols = symbols
+    self.states = states
+    self.io = io
+  end
+
+  def reduce
+    each_transition_goes_from_one_state
+    each_transition_goes_to_one_state
+    each_transition_reads_one_symbol
+    each_transition_writes_one_symbol
+
+    machine_is_deterministic
+  end
+
+  def each_transition_goes_from_one_state
+    transitions.times do |transition|
+      variables = state_vars("From", transition)
+      CommanderVariable.exactly_one(variables, io)
+    end
+  end
+
+  def each_transition_goes_to_one_state
+    transitions.times do |transition|
+      variables = state_vars("To", transition)
+      CommanderVariable.exactly_one(variables, io)
+    end
+  end
+
+  def each_transition_reads_one_symbol
+    transitions.times do |transition|
+      variables = symbol_vars("Read", transition)
+      CommanderVariable.exactly_one(variables, io)
+    end
+  end
+
+  def each_transition_writes_one_symbol
+    transitions.times do |transition|
+      variables = symbol_vars("Write", transition)
+      CommanderVariable.exactly_one(variables, io)
+    end
+  end
+
+  def machine_is_deterministic
+    # Create a variable for each possible from state / read symbol pair.
+    # From_st ^ Read_sy -> TransitionPair_st_sy
+    variables = transitions.times do |transition|
+      state_vars("From", transition).each.with_index do |from_state, i|
+        symbol_vars("Read", transition).each.with_index do |read_symbol, j|
+          variable = "TransitionPair_#{transition}_#{i}_#{j}"
+          io.puts "-#{from_state} -#{read_symbol} #{variable}"
+        end
+      end
+    end
+
+    # There must be one of each pairing across all transitions.
+    states.times do |state|
+      symbols.times do |symbol|
+        variables = transitions.times.map do |transition|
+          "TransitionPair_#{transition}_#{state}_#{symbol}"
+        end
+        CommanderVariable.exactly_one(variables, io)
+      end
+    end
+  end
+
+  def state_vars(prefix, transition)
+    states.times.map do |state|
+      "#{prefix}_#{transition}_#{state}"
+    end
+  end
+
+  def symbol_vars(prefix, transition)
+    symbols.times.map do |symbol|
+      "#{prefix}_#{transition}_#{symbol}"
+    end
+  end
+end
---
+++ spec/machine_maker/description_reducer_spec.rb
@@ -0,0 +1,174 @@
+RSpec.describe DescriptionReducer do
+  let(:io) { StringIO.new }
+  let(:dimacs) { io.string.split("\n") }
+  let(:transitions) { 2 }
+
+  subject do
+    described_class.new(
+      transitions: transitions,
+      symbols: 3,
+      states: 4,
+      io: io,
+    )
+  end
+
+  describe "#each_transition_goes_from_one_state" do
+    it "uses commander variable" do
+      subject.each_transition_goes_from_one_state
+
+      expect(dimacs).to include(
+        "From_1_0 From_1_1 From_1_2 -Com_3",
+      )
+    end
+  end
+
+  describe "#each_transition_goes_to_one_state" do
+    it "uses commander variable" do
+      subject.each_transition_goes_to_one_state
+
+      expect(dimacs).to include(
+        "To_1_0 To_1_1 To_1_2 -Com_3",
+      )
+    end
+  end
+
+  describe "#each_transition_reads_one_symbol" do
+    it "uses commander variable" do
+      subject.each_transition_reads_one_symbol
+
+      expect(dimacs).to include(
+        "-Read_1_0 -Read_1_2",
+      )
+    end
+  end
+
+  describe "#each_transition_writes_one_symbol" do
+    it "uses commander variable" do
+      subject.each_transition_writes_one_symbol
+
+      expect(dimacs).to include(
+        "-Write_1_0 -Write_1_2",
+      )
+    end
+  end
+
+  describe "#machine_is_deterministic" do
+    it "has a variable for ever from state/read symbol pair" do
+      subject.machine_is_deterministic
+
+      expect(dimacs).to include(
+        "-From_0_0 -Read_0_0 TransitionPair_0_0_0",
+        "-From_0_1 -Read_0_0 TransitionPair_0_1_0",
+        "-From_0_0 -Read_0_1 TransitionPair_0_0_1",
+        "-From_0_1 -Read_0_1 TransitionPair_0_1_1",
+        "-From_1_1 -Read_1_1 TransitionPair_1_1_1",
+      )
+    end
+
+    it "uses commander-variable across pairs with the same transition" do
+      subject.machine_is_deterministic
+
+      expect(dimacs).to include(
+        "TransitionPair_0_0_0 TransitionPair_1_0_0",
+        "-TransitionPair_0_0_0 -TransitionPair_1_0_0",
+
+        "TransitionPair_0_1_1 TransitionPair_1_1_1",
+        "-TransitionPair_0_1_1 -TransitionPair_1_1_1",
+      )
+
+      expect(dimacs).not_to include(
+        "TransitionPair_0_0_0 TransitionPair_0_0_0",
+        "TransitionPair_1_0_0 TransitionPair_1_0_0",
+        "TransitionPair_0_0_1 TransitionPair_1_1_0",
+      )
+    end
+  end
+
+  describe "integration" do
+    let(:dimacs) { StringIO.new }
+    let(:certificate) { StringIO.new }
+    let(:transitions) { 12 }
+
+    let(:result) do
+      subject.reduce
+
+      DimacsWriter.write(subject.io, dimacs, remember: true)
+      SatSolver.solve(dimacs, certificate)
+      CertificateDecoder.decode(certificate, dimacs)
+    end
+
+    it "has exactly one from state for a transition" do
+      states = result.values_at(
+        "From_0_0",
+        "From_0_1",
+        "From_0_2",
+        "From_0_3",
+      )
+
+      expect(states).to be_one
+    end
+
+    it "has exactly one to state for a transition" do
+      states = result.values_at(
+        "To_1_0",
+        "To_1_1",
+        "To_1_2",
+        "To_1_3",
+      )
+
+      expect(states).to be_one
+    end
+
+    it "has exactly one read symbol for a transition" do
+      symbols = result.values_at(
+        "Read_1_0",
+        "Read_1_1",
+        "Read_1_2",
+        "Read_1_3",
+      )
+
+      expect(symbols).to be_one
+    end
+
+    it "has exactly one write symbol for a transition" do
+      symbols = result.values_at(
+        "Write_0_0",
+        "Write_0_1",
+        "Write_0_2",
+        "Write_0_3",
+      )
+
+      expect(symbols).to be_one
+    end
+
+    it "is deterministic, i.e. it has one transition per pair" do
+      pairs = transitions.times.map do |transition|
+        from_states = result.values_at(
+          "From_#{transition}_0",
+          "From_#{transition}_1",
+          "From_#{transition}_2",
+          "From_#{transition}_3",
+        )
+
+        read_symbols = result.values_at(
+          "Read_#{transition}_0",
+          "Read_#{transition}_1",
+          "Read_#{transition}_2",
+          "Read_#{transition}_3",
+        )
+
+        [
+          from_states.index(true),
+          read_symbols.index(true),
+        ]
+      end
+
+      expect(pairs.sort).to eq [
+        [0, 0], [0, 1], [0, 2],
+        [1, 0], [1, 1], [1, 2],
+        [2, 0], [2, 1], [2, 2],
+        [3, 0], [3, 1], [3, 2],
+      ]
+    end
+  end
+end
